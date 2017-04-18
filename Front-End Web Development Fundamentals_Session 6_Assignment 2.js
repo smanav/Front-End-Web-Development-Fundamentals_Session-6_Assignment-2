7b7fb42bf67b94ce801d097ea551d185727e1f0f
smanav <smanav1991@gmail.com>
2017-04-18 16:12:33 +0530

    Add files via upload

---
+++ Front-End Web Development Fundamentals_Session 6_Assignment 2.js
@@ -0,0 +1,18 @@
+// Here is the example of greater number
+
+           var a=30, b=60, c=45;
+
+                    if(a>b && a>c)
+                           {
+	                         console.log("Greatest number is:" + a);
+                           }
+
+                    if(b>a && b>c)
+                           {
+	                         console.log("Greatest number is:" + b);
+                           }
+
+                    else
+                           {
+	                         console.log("Greatest number is:" + c);
+                           }
